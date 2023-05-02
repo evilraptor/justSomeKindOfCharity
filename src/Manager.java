9d2b265f8f12617b35f964691f5f0a451abf23ac
evilraptor <nikulin5.80mail@mail.ru>
2023-05-02 22:25:07 +0700

    GreatPotato0

--- src/Manager.java
+++ src/Manager.java
@@ -15,15 +15,41 @@ public class Manager {
         taskList.clear();
     }
 
-    public Task getById(int inputId) {
+    public Task getTaskById(int inputId) {
         for (int i = 0; i <= countOfTasks; i++) {
             if (taskList.get(i).getId() == inputId) return taskList.get(i);
         }
         return null;
     }
 
-    public void addTask(String inputName, String inputDescription, int inputId, String inputStatus) {
+    /*public void addTask(String inputName, String inputDescription, int inputId, String inputStatus) {
         taskList.add(countOfTasks, new Task(inputName, inputDescription, inputId, inputStatus));
         countOfTasks++;
+    }*/
+
+    public void addTask(Task task) {
+        taskList.add(countOfTasks, task);
+        countOfTasks++;
+    }
+
+    //TODO написать обновление таска (не понятно что же это значит)
+    public void updateTask(int previousTaskId, Task newTask) {
+        if (getTaskById(previousTaskId) != null) {
+            return;
+        } else {
+            deleteTaskById(previousTaskId);
+            addTask(newTask);
+        }
+    }
+    //FIXME возможно лишь возможно оно выглядит так??? здесь не понятно что делать если не было такого таска но я просто ретерню
+
+    public void deleteTaskById(int inputId) {
+        for (int i = 0; i <= countOfTasks; i++) {
+            if (taskList.get(i).getId() == inputId) {
+                taskList.remove(i);
+                countOfTasks--;
+                break;
+            }
+        }
     }
 }
