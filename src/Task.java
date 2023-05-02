c388005d8ea0c223249188938411f69b0b278d2f
evilraptor <nikulin5.80mail@mail.ru>
2023-05-02 22:36:02 +0700

    GreatPotato1

--- src/Task.java
+++ src/Task.java
@@ -6,6 +6,8 @@ public class Task {
     private String description;
     private int id;
     private String status;
+    private boolean isEpic;
+    private boolean isSubtask;
     private Collection<String> subTasks = new ArrayList<>();
 
     public Task() {
@@ -13,6 +15,17 @@ public class Task {
         description = "";
         id = -1;
         status = "";
+        isEpic = false;
+        isSubtask = false;
+    }
+
+    public Task(String inputName, String inputDescription, int inputId) {
+        name = inputName;
+        description = inputDescription;
+        id = inputId;
+        status = "NEW";
+        isEpic = false;
+        isSubtask = false;
     }
 
     public Task(String inputName, String inputDescription, int inputId, String inputStatus) {
@@ -20,8 +33,18 @@ public class Task {
         description = inputDescription;
         id = inputId;
         status = inputStatus;
+        isEpic=false;
+        isSubtask=false;
     }
-
+    //TODO подумать слишком большое количество аргументов-проблема? зато не приходится создавать 2 отдельных класс epic и subtask
+    public Task(String inputName, String inputDescription, int inputId, String inputStatus,boolean inputIsEpic,boolean inputIsSubtask) {
+        name = inputName;
+        description = inputDescription;
+        id = inputId;
+        status = inputStatus;
+        isEpic=inputIsEpic;
+        isSubtask=inputIsSubtask;
+    }
     public String getName() {
         return name;
     }
